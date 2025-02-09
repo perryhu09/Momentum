32498d599cf777de3fdd054bc9a1444835b82747
Victor <victorzhou10018@gmail.com>
2025-02-08 19:41:05 -0500

    database set up
    User db setup [x]
    
    routes:
    - /login
    - /register

--- backend/server/app.py
+++ backend/server/app.py
@@ -6,16 +6,41 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Successful Login'}), 200
+    return jsonify({'message': 'Invalid Credentials'}), 401
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email, password = data.get('email'), data.get('password')
+
+    if not email or not password:
+        return jsonify()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
